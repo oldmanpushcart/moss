d8b0addacdd1c23f8c91b6e49f2f7ff98f2959d8
oldmanpushcart <oldmanpushcart@gmail.com>
2025-04-17 01:29:42 +0800

    优化代码

--- src/main/java/io/github/oldmanpushcart/moss/backend/memory/internal/MemoryImpl.java
+++ src/main/java/io/github/oldmanpushcart/moss/backend/memory/internal/MemoryImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static io.github.oldmanpushcart.moss.backend.memory.internal.MemoryHelper.toMemoryFragment;
@@ -52,6 +53,7 @@ public class MemoryImpl implements Memory {
 
         }
 
+        Collections.sort(fragments);
         return fragments;
     }
 
